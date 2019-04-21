d3d8f5f95fefce495b2104574aecf906bf432a81
Rain Wang <rainwzp@gmail.com>
2019-04-21 11:23:55 +0800

    first commit

---
+++ macula-app-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  macula-app-ios
+//
+//  Created by Rain Wang on 2019/4/19.
+//  Copyright © 2019 Macula Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
